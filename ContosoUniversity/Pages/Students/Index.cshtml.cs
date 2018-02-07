89cd3cc5faf17e6b1320b7167d8ea6d1cd513278
Zuo Yang <zuya@microsoft.com>
2018-02-07 19:48:57 +0800

    Sorting, filtering, paging, and grouping - EF Core with Razor Pages

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -25,10 +25,21 @@ namespace ContosoUniversity.Pages.Students
         public string CurrentSort { get; set; }
         public PaginatedList<Student> Student { get;set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(
+            string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
             CurrentFilter = searchString;
 
             IQueryable<Student> studentIQ = from s in _context.Students
@@ -56,7 +67,10 @@ namespace ContosoUniversity.Pages.Students
                     break;
             }
 
-            Student = await studentIQ.AsNoTracking().ToListAsync();
+
+            int pageSize = 3;
+            Student = await PaginatedList<Student>.CreateAsync(
+                studentIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
